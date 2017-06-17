9c8bf53d61de602f9ddb5a3ede8719ddc10e3fa7
fahreeve <fahreeve@yandex.ru>
2017-06-17 20:35:24 +0300

    update version

--- aiovk/__init__.py
+++ aiovk/__init__.py
@@ -1,2 +1,4 @@
+__version__ = '1.2.0'
+
 from aiovk.sessions import ImplicitSession, TokenSession, AuthorizationCodeSession
 from aiovk.api import API
--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+import codecs
+import os
+import re
+
 from setuptools import setup, find_packages
 
 with open('README.rst', 'r') as f:
     readme = f.read()
 
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
+        __file__)), 'aiovk', '__init__.py'), 'r', 'latin1') as fp:
+    try:
+        version = re.findall(r"^__version__ = '([^']+)'\r?$",
+                             fp.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
 setup(
     name='aiovk',
-    version='1.1.1',
+    version=version,
 
     author='Fahreev Eldar',
     author_email='fahreeve@yandex.ru',
