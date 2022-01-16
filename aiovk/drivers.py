c26ed949455162cf0f4b4987ee174d621db2dc8d
Alexander Larin <ekzebox@gmail.com>
2022-01-16 19:10:11 +0300

    Inject headers to http drivers, add auth form referer which is required for auth now

--- aiovk/drivers.py
+++ aiovk/drivers.py
@@ -11,28 +11,28 @@ class BaseDriver:
         self.timeout = timeout
         self._loop = loop
 
-    async def post_json(self, url, params, timeout=None):
+    async def post_json(self, url, params, headers=None, timeout=None):
         """
         :param params: dict of query params
         :return: http status code, dict from json response
         """
         raise NotImplementedError
 
-    async def get_bin(self, url, params, timeout=None):
+    async def get_bin(self, url, params, headers=None, timeout=None):
         """
         :param params: dict of query params
         :return: http status code, binary body of response
         """
         raise NotImplementedError
 
-    async def get_text(self, url, params, timeout=None):
+    async def get_text(self, url, params, headers=None, timeout=None):
         """
         :param params: dict of query params
         :return: http status code, text body of response and redirect_url
         """
         raise NotImplementedError
 
-    async def post_text(self, url, data, timeout=None):
+    async def post_text(self, url, data, headers=None, timeout=None):
         """
         :param data: dict pr string
         :return: http status code, text body of response and redirect url
@@ -51,20 +51,20 @@ class HttpDriver(BaseDriver):
         else:
             self.session = session
 
-    async def post_json(self, url, params, timeout=None):
-        async with self.session.post(url, data=params, timeout=timeout or self.timeout) as response:
+    async def post_json(self, url, params, headers=None, timeout=None):
+        async with self.session.post(url, data=params, headers=headers, timeout=timeout or self.timeout) as response:
             return response.status, await response.json()
 
-    async def get_bin(self, url, params, timeout=None):
-        async with self.session.get(url, params=params, timeout=timeout or self.timeout) as response:
+    async def get_bin(self, url, params, headers=None, timeout=None):
+        async with self.session.get(url, params=params, headers=headers, timeout=timeout or self.timeout) as response:
             return response.status, await response.read()
 
-    async def get_text(self, url, params, timeout=None):
-        async with self.session.get(url, params=params, timeout=timeout or self.timeout) as response:
+    async def get_text(self, url, params, headers=None, timeout=None):
+        async with self.session.get(url, params=params, headers=headers, timeout=timeout or self.timeout) as response:
             return response.status, await response.text(), response.real_url
 
-    async def post_text(self, url, data, timeout=None):
-        async with self.session.post(url, data=data, timeout=timeout or self.timeout) as response:
+    async def post_text(self, url, data, headers=None, timeout=None):
+        async with self.session.post(url, data=data, headers=headers, timeout=timeout or self.timeout) as response:
             return response.status, await response.text(), response.real_url
 
     async def close(self):
